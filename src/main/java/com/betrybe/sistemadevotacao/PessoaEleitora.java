b8527f887ca5f6b3bfae7cec1bede1425113f4f4
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-25 15:29:41 -0300

    Requisito 3 ok

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,19 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }

578a09a3f8f0b5813ae19d3c2c9bf0945f27e9c9
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-25 14:42:07 -0300

    Requisito 1 ok

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,14 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+public abstract class Pessoa {
+
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }

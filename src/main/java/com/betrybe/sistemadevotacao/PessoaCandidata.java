5200f542fc7ea61ed4f196d477a4541cda08dfd7
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-25 15:04:53 -0300

    Requisito 2 ok

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,39 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * PessoaCandidata.
+ */
+public class PessoaCandidata extends Pessoa {
 
+  protected int numero;
+  protected int votos;
+  /**
+   * Construtor PessoaCandidata.
+   */
+
+  public PessoaCandidata(String nome, int numero) {
+    super.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }

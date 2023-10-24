b876c2045b4aad2ec679faff30c9116672095738
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-10-24 07:43:46 -0300

    feat: classe Principal

--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -1,10 +1,49 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.Scanner;
+
 public class Principal {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Scanner scanner = new Scanner(System.in);
+    boolean cadastrandoCandidato = true;
+    GerenciamentoVotacao votacao = new GerenciamentoVotacao();
 
+    while (cadastrandoCandidato) {
+      System.out.println("Cadastrar pessoa candidata?\n"
+          + "1 - Sim\n"
+          + "2 - Não\n"
+          + "Entre com o número correspondente à opção desejada: ");
+      int option = Integer.parseInt(scanner.nextLine());
+      if (option == 1) {
+        System.out.println("Entre com o nome da pessoa candidata: ");
+        String nomeCandidato = scanner.nextLine();
+
+        System.out.println("Entre com o número da pessoa candidata: ");
+        int numeroCandidato = Integer.parseInt(scanner.nextLine());
+        votacao.cadastrarPessoaCandidata(nomeCandidato, numeroCandidato);
+      } else {
+        cadastrandoCandidato = false;
+      }
+    }
+
+    boolean cadastrandoEleitor = true;
+    while (cadastrandoEleitor) {
+      System.out.println("Cadastrar pessoa eleitora?\n"
+          + "1 - Sim\n"
+          + "2 - Não\n"
+          + "Entre com o número correspondente à opção desejada: ");
+      int option = Integer.parseInt(scanner.nextLine());
+      if (option == 1) {
+        System.out.println("Entre com o nome da pessoa eleitora: ");
+        String nomeEleitor = scanner.nextLine();
+
+        System.out.println("Entre com o número da pessoa eleitora: ");
+        String cpfEleitor = scanner.nextLine();
+        votacao.cadastrarPessoaEleitora(nomeEleitor, cpfEleitor);
+      } else {
+        cadastrandoEleitor = false;
+      }
+    }
   }
-
 }
